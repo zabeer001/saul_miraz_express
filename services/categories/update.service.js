cd2221905c5f558957d0665e2a15200ba068fd09
zabeer001 <binzabirtareq@gmail.com>
2025-07-16 10:31:23 +0600

    e

--- services/categories/update.service.js
+++ services/categories/update.service.js
@@ -5,6 +5,7 @@ export const updateService = async (id, data) => {
     new: true,
     runValidators: true,
   });
+    category.image = await updateSingleImage(files['image'][0], category.image);
 
   if (!category) {
     throw new Error('Category not found');
