a1d6b864df435ad128da22369cf18b7d2cdc3bf5
zabeer001 <binzabirtareq@gmail.com>
2025-07-17 15:28:42 +0600

    register api

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,7 +7,7 @@ const authRouter = Router();
 
 // Existing routes
 authRouter.post('/login', upload.none(), AuthController.login);
-authRouter.post('/sign-up', upload.none(), AuthController.signUp);
+authRouter.post('/register', upload.none(), AuthController.signUp);
 authRouter.post('/logout', authenticate, AuthController.logout);
 authRouter.get('/me', authenticate, AuthController.profile);
 authRouter.post('/google/jwt-process', AuthController.loginWithGoogle)
